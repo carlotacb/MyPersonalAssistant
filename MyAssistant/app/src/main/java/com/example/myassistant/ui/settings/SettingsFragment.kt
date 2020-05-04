161a32b3b864d37643462a59119e7d6ffd2288d6
Carlota <carkbra@gmail.com>
2020-05-04 13:01:32 +0200

    created structure for settings fragment

---
+++ MyAssistant/app/src/main/java/com/example/myassistant/ui/settings/SettingsFragment.kt
@@ -0,0 +1,33 @@
+package com.example.myassistant.ui.settings
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import android.widget.TextView
+
+import com.example.myassistant.R
+
+class SettingsFragment : Fragment() {
+
+    private lateinit var settingsViewModel: SettingsViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        settingsViewModel =
+            ViewModelProviders.of(this).get(SettingsViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_settings, container, false)
+        val textView: TextView = root.findViewById(R.id.text_settings)
+        settingsViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+
+}
