ad7f74c9a165d2a467dde913f0ca70385b38bfec
Carlota <carkbra@gmail.com>
2020-05-05 12:35:24 +0200

    created speak logic

--- MyAssistant/app/src/main/java/com/example/myassistant/SpeakActivity.kt
+++ MyAssistant/app/src/main/java/com/example/myassistant/SpeakActivity.kt
@@ -1,9 +1,15 @@
 package com.example.myassistant
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.RecognizerIntent
+import android.widget.Button
 
 class SpeakActivity : AppCompatActivity() {
+
+    //SPEECH CODE
+    private var SPEECH_REQUEST_CODE: Int = 10
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,6 +19,9 @@ class SpeakActivity : AppCompatActivity() {
         val toolbar = supportActionBar
         toolbar!!.title = "Speak activity"
         toolbar.setDisplayHomeAsUpEnabled(true)
+
+        val speakButton: Button = findViewById(R.id.speak_button)
+        speakButton.setOnClickListener{ displaySpeechRecognizer() }
     }
 
     override fun onSupportNavigateUp(): Boolean {
@@ -20,4 +29,10 @@ class SpeakActivity : AppCompatActivity() {
         return true
     }
 
+    private fun displaySpeechRecognizer() {
+        val recognizerIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_PREFERENCE, "en")
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        startActivityForResult(intent, SPEECH_REQUEST_CODE)
+    }
 }
