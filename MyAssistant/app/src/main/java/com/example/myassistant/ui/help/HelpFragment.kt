6abd6d1324ae8f52a62384bb8a0cb1159cd0864b
Carlota <carkbra@gmail.com>
2020-05-13 16:17:40 +0200

    created Help Fragment

---
+++ MyAssistant/app/src/main/java/com/example/myassistant/ui/help/HelpFragment.kt
@@ -0,0 +1,21 @@
+package com.example.myassistant.ui.help
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.myassistant.R
+
+class HelpFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val root = inflater.inflate(R.layout.fragment_help, container, false)
+        return root
+    }
+
+}
